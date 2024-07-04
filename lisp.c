d6bb71bedf63c3bc912a3b664bc42412e51dc281
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-04 23:59:03 +0900

    Void implementation.

---
+++ lisp.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+__attribute__((noreturn))
+__attribute__((format(printf, 1, 2)))
+void throw(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    exit(2);
+}
+
+int main(int argc, char **argv)
+{
+    FILE *in = stdin;
+    if (argc > 1) {
+        in = fopen(argv[1], "r");
+        if (in == NULL)
+            throw("file %s not found\n", argv[1]);
+    }
+
+    return 0;
+}
