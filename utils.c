3471740044fe56d05fbde98c0f63e888b4bff2c2
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-06 11:10:56 +0900

    Add prefix for error message.

--- utils.c
+++ utils.c
@@ -8,6 +8,7 @@ void throw(const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+    fprintf(stderr, "error: ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(2);
