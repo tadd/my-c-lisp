f1c09873b25d59a0445a1217d70ea320025f8c07
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-09 00:03:20 +0900

    Add command line option "-e <src>".

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "lisp.h"
 #include "utils.h"
@@ -6,10 +7,20 @@
 int main(int argc, char **argv)
 {
     FILE *in = stdin;
-    if (argc > 1) {
+    switch (argc) {
+    case 3:
+        if (strcmp(argv[1], "-e") != 0)
+            break;
+        char *src = argv[2];
+        in = fmemopen(src, sizeof(src), "r");
+        break;
+    case 2:
         in = fopen(argv[1], "r");
         if (in == NULL)
             error("file %s not found", argv[1]);
+        break;
+    default:
+        break;
     }
     Value v = parse(in);
     if (value_is_nil(v)) {
@@ -20,5 +31,7 @@ int main(int argc, char **argv)
         printf("\n");
         v = cdr(v);
     } while (!value_is_nil(v));
+
+    fclose(in);
     return 0;
 }
