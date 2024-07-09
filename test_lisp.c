e7e77a23f5f56ec82550ac1c3a8c72d0d359759a
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-09 19:21:31 +0900

    Add tests.

--- test_lisp.c
+++ test_lisp.c
@@ -11,7 +11,19 @@ Test(lisp, nil) {
     cr_assert(value_is_nil(a));
 }
 
-Test(lisp, parse) {
-    Value a = parse_expr_from_string("42");
-    cr_assert(eq(42, value_to_int(a)));
+Test(lisp, parse_int) {
+    Value v = parse_expr_from_string("42");
+    cr_assert(eq(42, value_to_int(v)));
+}
+
+Test(lisp, parse_nil) {
+    Value v = parse_expr_from_string("()");
+    cr_assert(value_is_nil(v));
+}
+
+Test(lisp, parse_list) {
+    Value v = parse_expr_from_string("(1 2)");
+    cr_assert(eq(1, value_to_int(car(v))));
+    cr_assert(eq(2, value_to_int(car(cdr(v)))));
+    cr_assert(value_is_nil(cdr(cdr(v))));
 }
