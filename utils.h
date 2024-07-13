c7802ba41e0dd58cd713bf3b2fd19b0d4d5a46e5
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-13 11:11:41 +0900

    Back to C17 because GCC 12 do not support func(...) for no arguments.

--- utils.h
+++ utils.h
@@ -3,11 +3,13 @@
 
 #include <stdlib.h>
 
-#define ATTR_UNUSED [[unused]]
-#define ATTR_NORETURN [[gnu::noreturn]]
-#define ATTR_MALLOC [[gnu::malloc]] [[gnu::used]]
-#define ATTR_XMALLOC ATTR_MALLOC [[gnu::nonnull]]
-#define ATTR_FORMAT(f, beg, end) [[gnu::format(f, beg, end)]]
+#define ATTR(x) __attribute__((x))
+#define UNREACHABLE() __builtin_unreachable()
+#define ATTR_UNUSED ATTR(unused)
+#define ATTR_NORETURN ATTR(noreturn)
+#define ATTR_MALLOC ATTR(malloc) ATTR(used)
+#define ATTR_XMALLOC ATTR_MALLOC ATTR(nonnull)
+#define ATTR_FORMAT(f, beg, end) ATTR(format(f, beg, end))
 
 ATTR_NORETURN ATTR_FORMAT(printf, 1, 2) void error(const char *fmt, ...);
 ATTR_XMALLOC void *xmalloc(size_t size);
