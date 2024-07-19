634cf3ea584250996903d2b19717f846f61a9086
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-19 14:07:03 +0900

    Drop newer but experimental style assertions.

--- test_lisp.c
+++ test_lisp.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 #include <criterion/criterion.h>
-#include <criterion/new/assert.h>
-#define streq(x, y) eq(str, (char *)x, (char *)y)
 
 #include "lisp.h"
 
@@ -14,7 +12,7 @@ Test(lisp, nil) {
 
 #define assert_stringify(expected, v) do { \
         char *s = stringify(v); \
-        cr_assert(streq(expected, s)); \
+        cr_assert_str_eq(expected, s); \
         free(s); \
     } while (0)
 
@@ -39,11 +37,11 @@ Test(lisp, printing) {
 
 Test(lisp, parse_int) {
     Value v = parse_expr_string("42");
-    cr_assert(eq(42, value_to_int(v)));
+    cr_assert_eq(42, value_to_int(v));
 
     v = parse_expr_string("-42");
-    cr_assert(eq(int, -42, value_to_int(v)));
-    cr_assert(streq("-42", stringify(v)));
+    cr_assert_eq(-42, value_to_int(v));
+    cr_assert_str_eq("-42", stringify(v));
 }
 
 Test(lisp, parse_nil) {
@@ -53,8 +51,8 @@ Test(lisp, parse_nil) {
 
 Test(lisp, parse_list) {
     Value v = parse_expr_string("(1 2)");
-    cr_assert(eq(1, value_to_int(car(v))));
-    cr_assert(eq(2, value_to_int(cadr(v))));
+    cr_assert_eq(1, value_to_int(car(v)));
+    cr_assert_eq(2, value_to_int(cadr(v)));
     cr_assert(value_is_nil(cddr(v)));
 }
 
@@ -62,20 +60,20 @@ Test(lisp, cxr) {
     Value v = parse_expr_string("((((42))))");
     Value i = caaaar(v);
     cr_assert(value_is_int(i));
-    cr_assert(eq(42, value_to_int(i)));
+    cr_assert_eq(42, value_to_int(i));
 }
 
 Test(lisp, parse_ident) {
     Value v = parse_expr_string("a");
     cr_assert(value_is_symbol(v));
-    cr_assert(streq("a", value_to_string(v)));
+    cr_assert_str_eq("a", value_to_string(v));
 }
 
 Test(lisp, parse_dot) {
     Value v2 = parse_expr_string("(1 . 2)");
     cr_assert(value_is_pair(v2));
-    cr_assert(eq(int, 1, value_to_int(car(v2))));
-    cr_assert(eq(int, 2, value_to_int(cdr(v2))));
+    cr_assert_eq(1, value_to_int(car(v2)));
+    cr_assert_eq(2, value_to_int(cdr(v2)));
 }
 
 Test(lisp, parse_peculiar) {
@@ -89,106 +87,106 @@ Test(lisp, parse_peculiar) {
 Test(lisp, eval_arithmetic_literal) {
     Value v = eval_string("(+ 42 21)");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, 63, value_to_int(v)));
+    cr_assert_eq(63, value_to_int(v));
 
     v = eval_string("(- 42 21)");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, 21, value_to_int(v)));
+    cr_assert_eq(21, value_to_int(v));
 
     v = eval_string("(* 4 2)");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, 8, value_to_int(v)));
+    cr_assert_eq(8, value_to_int(v));
 
     v = eval_string("(/ 4 2)");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, 2, value_to_int(v)));
+    cr_assert_eq(2, value_to_int(v));
 }
 
 Test(lisp, eval_arithmetic_expr) {
     Value v = eval_string("(+ (+ 40 2) 21)");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, 63, value_to_int(v)));
+    cr_assert_eq(63, value_to_int(v));
 
     v = eval_string("(+ (- 40 4) (* 3 (/ 100 50)))");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, 42, value_to_int(v)));
+    cr_assert_eq(42, value_to_int(v));
 }
 
 Test(lisp, unbound_variable) {
     Value v = eval_string("x");
-    cr_assert(eq(v, Qundef));
+    cr_assert_eq(v, Qundef);
 
     v = eval_string("(+ x 2)");
-    cr_assert(eq(v, Qundef));
+    cr_assert_eq(v, Qundef);
 }
 
 Test(lisp, true_false) {
     Value v = eval_string("#t");
-    cr_assert(eq(v, Qtrue));
+    cr_assert_eq(v, Qtrue);
     v = eval_string("#f");
-    cr_assert(eq(v, Qfalse));
+    cr_assert_eq(v, Qfalse);
 }
 
 Test(lisp, if) {
     Value v;
     v = eval_string("(if #t 1)");
-    cr_assert(eq(1, value_to_int(v)));
+    cr_assert_eq(1, value_to_int(v));
 
 #if 0 // undefined in R^5RS, maybe
     v = eval_string("(if #f 1)");
-    cr_assert(eq(Qfalse, v));
+    cr_assert_eq(Qfalse, v));
 #endif
 
     v = eval_string("(if #t 1 2)");
-    cr_assert(eq(1, value_to_int(v)));
+    cr_assert_eq(1, value_to_int(v));
 
     v = eval_string("(if #f 1 2)");
-    cr_assert(eq(2, value_to_int(v)));
+    cr_assert_eq(2, value_to_int(v));
 }
 
 Test(lisp, if_composed) {
     Value v;
     v = eval_string("(if (if #t 1 #f) (if #t 3 4) (if #t 5 6))");
-    cr_assert(eq(3, value_to_int(v)));
+    cr_assert_eq(3, value_to_int(v));
 
     v = eval_string("(if (if #f 1 #f) (if #f 3 4) (if #f 5 6))");
-    cr_assert(eq(6, value_to_int(v)));
+    cr_assert_eq(6, value_to_int(v));
 }
 
 Test(lisp, list) {
     Value v;
     v = list(Qundef);
-    cr_assert(eq(v, Qnil));
+    cr_assert_eq(v, Qnil);
 
     v = list(value_of_int(42), Qundef);
     cr_assert(value_is_pair(v));
-    cr_assert(eq(1, length(v)));
+    cr_assert_eq(1, length(v));
     cr_assert(value_is_int(car(v)));
-    cr_assert(eq(42, value_to_int(car(v))));
+    cr_assert_eq(42, value_to_int(car(v)));
 
     v = list(value_of_int(42),
              value_of_symbol("foo"),
              value_of_func(value_of_func, 0),
              Qundef);
     cr_assert(value_is_pair(v));
-    cr_assert(eq(3, length(v)));
+    cr_assert_eq(3, length(v));
     Value v0 = car(v);
     cr_assert(value_is_int(v0));
-    cr_assert(eq(42, value_to_int(v0)));
+    cr_assert_eq(42, value_to_int(v0));
     Value v1 = cadr(v);
     cr_assert(value_is_symbol(v1));
-    cr_assert(streq("foo", value_to_string(v1)));
+    cr_assert_str_eq("foo", value_to_string(v1));
     Value v2 = caddr(v);
     cr_assert(value_is_func(v2));
-    cr_assert(streq("<function>", stringify(v2)));
+    cr_assert_str_eq("<function>", stringify(v2));
 }
 
 #define assert_list_eq(expected, actual) do { \
         Value exp = expected, act = actual; \
         cr_assert(value_is_pair(act)); \
-        cr_assert(eq(long, length(exp), length(act))); \
+        cr_assert_eq(length(exp), length(act)); \
         for (; exp != Qnil; exp = cdr(exp), act = cdr(act)) \
-            cr_assert(eq(int, value_to_int(car(exp)), value_to_int(car(act)))); \
+            cr_assert_eq(value_to_int(car(exp)), value_to_int(car(act))); \
     } while (0)
 #define V(x) _Generic(x, int: value_of_int(x), const char *: value_of_symbol, Value: x)
 
@@ -206,13 +204,13 @@ Test(lisp, define_variable) {
     Value v;
     v = eval_string("(define x 42)");
     cr_assert(value_is_symbol(v));
-    cr_assert(streq("x", value_to_string(v)));
+    cr_assert_str_eq("x", value_to_string(v));
 
     v = eval_string("x");
     cr_assert(value_is_int(v));
-    cr_assert(eq(42, value_to_int(v)));
+    cr_assert_eq(42, value_to_int(v));
 
     v = eval_string("(define x (* -1 42)) x");
     cr_assert(value_is_int(v));
-    cr_assert(eq(int, -42, value_to_int(v)));
+    cr_assert_eq(-42, value_to_int(v));
 }
