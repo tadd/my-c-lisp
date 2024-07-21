0b7f40fdfd3174cc87e2a9bd602deec447ee0c22
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-21 21:39:38 +0900

    Working on

--- lisp.c
+++ lisp.c
@@ -1675,73 +1675,75 @@ static Value proc_integer_p(UNUSED Value *env, Value obj)
     return OF_BOOL(value_is_int(obj));
 }
 
-static Value proc_numeq(UNUSED Value *env, Value args)
-{
-    expect_arity_range("=", 2, -1, args);
+typedef enum {
+    RELOP_EQ,
+    RELOP_LT,
+    RELOP_GT,
+    RELOP_LE,
+    RELOP_GE,
+} RelOp;
 
-    int64_t x = value_get_int("=", car(args));
+typedef bool (*RelOpFunc)(int64_t x, int64_t y);
+static inline bool relop_eq(int64_t x, int64_t y) { return x == y; }
+static inline bool relop_lt(int64_t x, int64_t y) { return x <  y; }
+static inline bool relop_le(int64_t x, int64_t y) { return x <= y; }
+static inline bool relop_gt(int64_t x, int64_t y) { return x >  y; }
+static inline bool relop_ge(int64_t x, int64_t y) { return x >= y; }
+
+static const char *const relop_names[] = {
+    [RELOP_EQ] = "=",
+    [RELOP_LT] = "<",
+    [RELOP_GT] = ">",
+    [RELOP_LE] = "<=",
+    [RELOP_GE] = ">=",
+};
+static const RelOpFunc relop_funcs[]= {
+    [RELOP_EQ] = relop_eq,
+    [RELOP_LT] = relop_lt,
+    [RELOP_GT] = relop_gt,
+    [RELOP_LE] = relop_le,
+    [RELOP_GE] = relop_ge,
+};
+
+static Value relop(const char *name, RelOpFunc func, Value args)
+{
+    expect_arity_range(name, 2, -1, args);
+
+    int64_t x = value_get_int(name, car(args));
+
     while ((args = cdr(args)) != Qnil) {
-        int64_t y = value_get_int("=", car(args));
-        if (x != y)
+        int64_t y = value_get_int(name, car(args));
+        if (!func(x, y))
             return Qfalse;
+        x = y;
     }
     return Qtrue;
+}
+
+
+static Value proc_numeq(UNUSED Value *env, Value args)
+{
+    return relop(relop_names[RELOP_EQ], relop_funcs[RELOP_EQ], args);
 }
 
 static Value proc_lt(UNUSED Value *env, Value args)
 {
-    expect_arity_range("<", 2, -1, args);
-
-    int64_t x = value_get_int("<", car(args));
-    while ((args = cdr(args)) != Qnil) {
-        int64_t y = value_get_int("<", car(args));
-        if (x >= y)
-            return Qfalse;
-        x = y;
-    }
-    return Qtrue;
+    return relop(relop_names[RELOP_LT], relop_funcs[RELOP_LT], args);
 }
 
 static Value proc_gt(UNUSED Value *env, Value args)
 {
-    expect_arity_range(">", 2, -1, args);
-
-    int64_t x = value_get_int(">", car(args));
-    while ((args = cdr(args)) != Qnil) {
-        int64_t y = value_get_int(">", car(args));
-        if (x <= y)
-            return Qfalse;
-        x = y;
-    }
-    return Qtrue;
+    return relop(relop_names[RELOP_GT], relop_funcs[RELOP_GT], args);
 }
 
 static Value proc_le(UNUSED Value *env, Value args)
 {
-    expect_arity_range("<=", 2, -1, args);
-
-    int64_t x = value_get_int("<=", car(args));
-    while ((args = cdr(args)) != Qnil) {
-        int64_t y = value_get_int("<=", car(args));
-        if (x > y)
-            return Qfalse;
-        x = y;
-    }
-    return Qtrue;
+    return relop(relop_names[RELOP_LE], relop_funcs[RELOP_LE], args);
 }
 
 static Value proc_ge(UNUSED Value *env, Value args)
 {
-    expect_arity_range(">=", 2, -1, args);
-
-    int64_t x = value_get_int(">=", car(args));
-    while ((args = cdr(args)) != Qnil) {
-        int64_t y = value_get_int(">=", car(args));
-        if (x < y)
-            return Qfalse;
-        x = y;
-    }
-    return Qtrue;
+    return relop(relop_names[RELOP_GE], relop_funcs[RELOP_GE], args);
 }
 
 static Value proc_zero_p(UNUSED Value *env, Value obj)
