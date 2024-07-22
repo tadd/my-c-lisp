a19078ea1cf18835ea9b39cc2caa9f3689e361c3
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-22 13:37:14 +0900

    Usage.

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +6,24 @@
 
 #include "lisp.h"
 #include "utils.h"
+
+ATTR_NORETURN
+static void usage(FILE *out)
+{
+    fprintf(out, "Usage: lisp <file.scm>\n");
+    fprintf(out, "   or: lisp -e '(direct (program (here)))'\n");
+    exit(out == stdout ? 0 : 2);
+}
+
+static void opt_error(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    fprintf(stderr, "error: ");
+    vfprintf(stderr, fmt, ap);
+    va_end(ap);
+    usage(stderr);
+}
 
 typedef struct {
     FILE *in;
@@ -15,28 +34,33 @@ static Option parse_opt(int argc, char *const *argv)
 {
     Option o = { .in = NULL, .print = false };
     int opt;
-    while ((opt = getopt(argc, argv, "e:p")) != -1) {
+    while ((opt = getopt(argc, argv, "e:hp")) != -1) {
         switch (opt) {
         case 'e':
             o.in = fmemopen(optarg, strlen(optarg), "r");
             break;
+        case 'h':
+            usage(stdout);
         case 'p':
             o.print = true;
             break;
         case '?':
-            exit(2);
+            usage(stderr);
         }
     }
     const char *f = argv[optind];
-    if (f) {
-        if (o.in != NULL)
-            error("filename %s given while option '-e' passed", f);
+    if (f == NULL && o.in == NULL)
+        opt_error("no program provided\n");
+    if (f != NULL && o.in != NULL)
+        opt_error("filename %s given while option '-e' passed\n", f);
+    if (f != NULL) {
         o.in = fopen(f, "r");
         if (o.in == NULL)
-            error("can't read file %s", f);
+            opt_error("can't read file %s", f);
     }
     return o;
 }
+
 int main(int argc, char **argv)
 {
     Option opt = parse_opt(argc, argv);
