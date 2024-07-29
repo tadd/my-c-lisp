33aa010007c73cbfd81f8ee63855b244ede7a6e8
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-29 21:51:47 +0900

    Add pretty printter for gdb.

---
+++ pp.py
@@ -0,0 +1,41 @@
+def highlight(s):
+    return "\033[36m" + s + "\033[m"
+
+def cfuncall(name, *args):
+    func = gdb.lookup_global_symbol(name).value()
+    return func(*args)
+
+class ValuePrinter:
+    TYPE = gdb.lookup_type('Value')
+    TAG_LABELS = '''
+    TAG_PAIR
+    TAG_STR
+    TAG_CFUNC
+    TAG_SPECIAL
+    TAG_CLOSURE
+    '''.split()
+    TAGS = dict([[l, int(gdb.lookup_static_symbol(l).value())] for l in TAG_LABELS])
+
+    def __init__(self, val):
+        self.val = val
+        #self.tag = int(cfuncall('VALUE_TAG', val));
+
+    def to_string(self):
+        return cfuncall('stringify', self.val)
+
+class PP (gdb.Command):
+    def __init__(self):
+        super(type(self), self).__init__('pp', gdb.COMMAND_DATA)
+    def invoke(self, arg, from_tty):
+        val = gdb.parse_and_eval(arg)
+        pp = my_c_lisp_pp(val)
+        if pp:
+            print(pp.to_string())
+        else:
+            print(str(val))
+PP()
+
+def my_c_lisp_pp(val):
+    return ValuePrinter(val)
+
+#gdb.pretty_printers.append(my_c_lisp_pp) # gdb dumps core!!
