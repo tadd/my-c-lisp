b2e5cfbb6563f8b36b3c8715096794b33c8fc898
Tadashi Saito <tad.a.digger@gmail.com>
2024-09-22 02:24:40 +0900

    Implement hash table.

--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,6 +15,12 @@ ATTR(noreturn) ATTR(format(printf, 1, 2)) void error(const char *fmt, ...);
 ATTR_XMALLOC void *xmalloc(size_t size);
 ATTR_XMALLOC void *xrealloc(void *p, size_t size);
 ATTR_XMALLOC char *xstrdup(const char *s);
+
+typedef struct Table Table;
+Table *table_new(void);
+void table_free(Table *t);
+void table_put(Table *t, uint64_t key, uint64_t val); // `val` can't be 0
+uint64_t table_get(const Table *t, uint64_t key);
 
 #define debug(fmt, ...) fprintf(stderr, fmt "\n" __VA_OPT__(,) __VA_ARGS__);
 
