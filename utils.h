204688a822d20dbcb75432c02de6cb0f0d4c729e
Tadashi Saito <tad.a.digger@gmail.com>
2024-10-12 21:38:37 +0900

    Add an utility func.

--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 #define ATTR(x) __attribute__((x))
@@ -12,5 +13,7 @@ ATTR(noreturn) ATTR(format(printf, 1, 2)) void error(const char *fmt, ...);
 ATTR_XMALLOC void *xmalloc(size_t size);
 ATTR_XMALLOC void *xrealloc(void *p, size_t size);
 ATTR_XMALLOC char *xstrdup(const char *s);
+
+#define debug(fmt, ...) fprintf(stderr, fmt "\n" __VA_OPT__(,) __VA_ARGS__);
 
 #endif
