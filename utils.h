371591240f173ae9d1052619be42d478e28a23e6
Tadashi Saito <tad.a.digger@gmail.com>
2024-10-20 20:32:39 +0900

    Add table_set_or_put which set value if key found or just put.

--- utils.h
+++ utils.h
@@ -28,6 +28,7 @@ void table_free(Table *t);
 void table_put(Table *t, uint64_t key, uint64_t val); // `val` can't be 0
 uint64_t table_get(const Table *t, uint64_t key);
 bool table_set(Table *t, uint64_t key, uint64_t val); // set if found
+bool table_set_or_put(Table *t, uint64_t key, uint64_t val);
 void table_merge(Table *dst, const Table *src);
 
 #define debug(fmt, ...) fprintf(stderr, fmt "\n" __VA_OPT__(,) __VA_ARGS__);
