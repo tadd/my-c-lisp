2a6d007d3d31edb4bb8ea9907ede47d39c2c7675
Tadashi Saito <tad.a.digger@gmail.com>
2024-10-20 19:04:22 +0900

    Table: add free functions for keys.

--- utils.h
+++ utils.h
@@ -19,9 +19,10 @@ ATTR_XMALLOC char *xstrdup(const char *s);
 typedef struct Table Table;
 typedef bool (*TableEqualFunc)(uint64_t x, uint64_t y);
 typedef uint64_t (*TableHashFunc)(uint64_t x);
+typedef void (*TableFreeFunc)(void *p);
 Table *table_new(void);
 Table *table_new_str(void);
-Table *table_new_full(TableHashFunc hash, TableEqualFunc eq);
+Table *table_new_full(TableHashFunc hash, TableEqualFunc eq, TableFreeFunc free_key);
 Table *table_inherit(const Table *t);
 void table_free(Table *t);
 void table_put(Table *t, uint64_t key, uint64_t val); // `val` can't be 0
