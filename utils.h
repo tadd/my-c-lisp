361330415151f266b4d9c5b41551537996682559
Tadashi Saito <tad.a.digger@gmail.com>
2024-10-20 17:58:02 +0900

    Implement table_inherit, a view-table with their own key/value pairs.

--- utils.h
+++ utils.h
@@ -22,6 +22,7 @@ typedef uint64_t (*TableHashFunc)(uint64_t x);
 Table *table_new(void);
 Table *table_new_str(void);
 Table *table_new_full(TableHashFunc hash, TableEqualFunc eq);
+Table *table_inherit(const Table *t);
 void table_free(Table *t);
 void table_put(Table *t, uint64_t key, uint64_t val); // `val` can't be 0
 uint64_t table_get(const Table *t, uint64_t key);
