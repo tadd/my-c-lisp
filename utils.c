8a2eea9b750796db4de68cc463ee1047dc395e7b
Tadashi Saito <tad.a.digger@gmail.com>
2024-10-26 17:30:32 +0900

    Tweak semantics of table_merge.

--- utils.c
+++ utils.c
@@ -331,6 +331,6 @@ void table_merge(Table *dst, const Table *src)
     const size_t size = src->body_size;
     for (size_t i = 0; i < size; i++) {
         for (List *l = src->body[i]; l != NULL; l = l->next)
-            table_put(dst, l->key, l->value);
+            table_set_or_put(dst, l->key, l->value);
     }
 }
