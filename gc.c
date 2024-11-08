bb52ca9bc2e35ac55fa5e3bbe851a7e0e7e8eebf
Tadashi Saito <tad.a.digger@gmail.com>
2024-11-08 22:30:55 +0900

    Working on GC: Just allocate.

--- gc.c
+++ gc.c
@@ -1,26 +1,98 @@
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "gc.h"
 #include "utils.h"
+#include "schaf.h"
+
+#define HEADER(v) ((Header *) v)
+
+typedef struct {
+    size_t size;
+    bool allocated;
+    bool living;
+} Header;
+
+typedef struct Chunk {
+    Header h;
+    struct Chunk *next;
+} Chunk;
+
+static const size_t INIT_SIZE = 25 * (1024 * 1024); // MiB
+static void *heap;
+static Chunk free_list[1];
+
+void gc_init(void)
+{
+    heap = malloc(INIT_SIZE);
+    if (heap == NULL)
+        error("out of memory; initial malloc(%zu) failed", INIT_SIZE);
+    Chunk *ch = heap;
+    ch->h.size = INIT_SIZE - sizeof(Header);
+    ch->h.allocated = false;
+    ch->h.living = false;
+    ch->next = NULL;
+    free_list->next = ch; // free_list itself is never used as a chunk
+}
+
+static void *allocate_from_list(Chunk *prev, Chunk *curr, size_t size)
+{
+    uint8_t *p = (uint8_t *) curr;
+    size_t hsize = size + sizeof(Header);
+    if (curr->h.size == hsize)
+        prev->next = curr->next;
+    else {
+        Header h = curr->h;
+        h.size -= hsize;
+        Chunk *next = curr->next;
+        Chunk *ch = (Chunk *)(p + hsize);
+        ch->h = h;
+        ch->next = next;
+        prev->next = ch;
+    }
+    Header *o = HEADER(p);
+    o->size = size;
+    o->allocated = true;
+    return o + 1; // user of allocation use curr->next space and so-on
+}
+
+static void *allocate(size_t size)
+{
+    size_t hsize = size + sizeof(Header);
+    for (Chunk *prev = free_list, *curr; (curr = prev->next) != NULL; prev = curr) {
+        if (curr->h.size >= hsize) // First-fit
+            return allocate_from_list(prev, curr, size);
+    }
+    return NULL;
+}
 
 void *xmalloc(size_t size)
 {
-    void *p = malloc(size);
+    void *p = allocate(size);
     if (p == NULL)
-        error("malloc(%zu) failed", size);
+        error("out of memory; %s(%zu) failed", __func__, size);
     return p;
 }
 
 void *xrealloc(void *q, size_t size)
 {
-    void *p = realloc(q, size);
+    void *p = allocate(size);
     if (p == NULL)
-        error("realloc(ptr, %zu) failed", size);
+        error("out of memory; %s(ptr, %zu) failed", __func__, size);
+    Header *op = p, *oq = q;
+    memcpy(p, q, oq->size);
+    xfree(q);
+    op->size = size;
     return p;
 }
 
 void xfree(void *p)
 {
-    free(p); // as-is for now
+    Chunk *ch = p;
+    ch->h.allocated = false;
+    ch->next = free_list->next; // prepend
+    free_list->next = ch;
 }
--- gc.h
+++ gc.h
@@ -1,8 +1,68 @@
 #ifndef GC_H
 #define GC_H
 
+#include <setjmp.h>
 #include <stdlib.h>
 
 #include "utils.h"
+#include "schaf.h"
+
+#define VALUE_TAG(v) (*(ValueTag *)(v))
+
+#define PAIR(v) ((Pair *) v)
+#define STRING(v) ((String *) v)
+#define PROCEDURE(v) ((Procedure *) v)
+#define CFUNC(v) ((CFunc *) v)
+#define CLOSURE(v) ((Closure *) v)
+#define CONTINUATION(v) ((Continuation *) v)
+
+typedef enum { // has the same values as Type
+    TAG_PAIR  = TYPE_PAIR,
+    TAG_STR   = TYPE_STR,
+    TAG_CFUNC = TYPE_PROC + 1,
+    TAG_SYNTAX, // almost a C Function
+    TAG_CLOSURE,
+    TAG_CONTINUATION,
+} ValueTag;
+
+typedef struct Pair {
+    ValueTag tag; // common
+    Value car, cdr;
+} Pair;
+
+typedef struct {
+    ValueTag tag;
+    char body[];
+} String;
+
+typedef struct {
+    ValueTag tag;
+    int64_t arity;
+} Procedure;
+
+typedef Value (*cfunc_t)(/*ANYARGS*/);
+typedef struct {
+    Procedure proc;
+    cfunc_t cfunc;
+} CFunc;
+
+typedef struct {
+    Procedure proc;
+    Value env;
+    Value params;
+    Value body;
+} Closure;
+
+typedef struct {
+    Procedure proc;
+    volatile void *sp;
+    void *shelter;
+    size_t shelter_len;
+    Value call_stack;
+    jmp_buf state;
+    Value retval;
+} Continuation;
+
+void gc_init(void);
 
 #endif
